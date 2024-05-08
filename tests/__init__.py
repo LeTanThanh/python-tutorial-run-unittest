31e3b3cef3280b3a40dfefee68a2fb37c57ca560
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-08 13:28:49 +0700

    Append sys.path in tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,13 @@
 import sys
 
-from .path import SHAPES_PATH
+import os
+
+file_path = __file__
+paths = file_path.split("/")
+paths.pop() # remove __init__.py
+paths.pop() # remove tests
+
+ROOT_PATH = "/".join(paths)
+SHAPES_PATH = f"{ROOT_PATH}/shapes"
 
 sys.path.append(SHAPES_PATH)
--- tests/path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-file_path = __file__
-paths = file_path.split("/")
-paths.pop() # remove path
-paths.pop() # remove test
-
-ROOT_PATH = "/".join(paths)
-SHAPES_PATH = f"{ROOT_PATH}/shapes"
